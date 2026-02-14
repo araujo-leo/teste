0642269e9f42479a01535dbe4cf22e0fab5f0a0f
leonardo araujo <leoaraujofnd@gmail.com>
2026-02-13 23:19:12 -0300

    fixing logout

--- views/partials/header.php
+++ views/partials/header.php
@@ -163,6 +163,7 @@
                 e.preventDefault();
                 localStorage.removeItem('auth_token');
                 localStorage.removeItem('user');
+                document.cookie = 'auth_token=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT';
                 window.location.href = '/login';
             });
         });
